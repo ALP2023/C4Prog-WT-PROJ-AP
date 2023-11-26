01eca2a318eda56f04e3fdd571ef0cca4e33fc4b
April Pickard <aprillpickard@gmail.com>
2023-11-26 20:42:26 +0800

    feat: Projects page completed.

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,4 +1,4 @@
-// List of image paths in the about directory
+// List of image paths in the about & projects directories
 const imagePaths = [
     'pic01.jpg',
     'pic02.jpg',
@@ -6,19 +6,51 @@ const imagePaths = [
     'pic04.jpg',
     'pic05.jpg',
     'pic06.jpg',
+    'pic07.jpg',
+    'pic08.jpg',
+    'pic09.jpg',
+    'pic10.jpg',
+    'pic11.jpg',
+    'pic12.jpg',
 ];
 
-let currentImageIndex = 0;
+const imagePaths2 = [
+    'csharp01.jpg',
+    'csharp02.jpg',
+    'csharp03.jpg',
+];
+
+const imagePaths3 = [
+    'sql01.jpg',
+    'sql02.jpg',
+    'sql03.jpg',
+];
+
+let currentAboutImageIndex = 0;
 const aboutImage = document.getElementById('about-image');
 
-// Function to change the image source
-function changeImage() {
-    if (currentImageIndex === imagePaths.length) {
-        currentImageIndex = 0; // Reset the index to loop through images
-    }
-    aboutImage.src = `assets/img/about/${imagePaths[currentImageIndex]}`;
-    currentImageIndex++;
+let currentCsharpImageIndex = 0;
+const csharpImage = document.getElementById('csharp-image');
+
+let currentSqlImageIndex = 0;
+const sqlImage = document.getElementById('sql-image');
+
+function changeAboutImage() {
+    aboutImage.src = `assets/img/about/${imagePaths[currentAboutImageIndex]}`;
+    currentAboutImageIndex = (currentAboutImageIndex + 1) % imagePaths.length;
 }
 
-// Change image every 4 seconds
-setInterval(changeImage, 4000);
+function changeCsharpImage() {
+    csharpImage.src = `assets/img/projects/${imagePaths2[currentCsharpImageIndex]}`;
+    currentCsharpImageIndex = (currentCsharpImageIndex + 1) % imagePaths2.length;
+}
+
+function changeSqlImage() {
+    sqlImage.src = `assets/img/projects/${imagePaths3[currentSqlImageIndex]}`;
+    currentSqlImageIndex = (currentSqlImageIndex + 1) % imagePaths3.length;
+}
+
+// Change images every 4 seconds for about-image and project-image respectively
+setInterval(changeAboutImage, 4000);
+setInterval(changeCsharpImage, 3500);
+setInterval(changeSqlImage, 3500);
